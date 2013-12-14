665c27342d82b7ee37b988b5d344a183ea8457ba
Carlos G. Gavidia <cgavidia@acm.org>
2013-12-14 17:51:53 -0500

    Utility methods

--- AnotherACOImageSegmentation/src/pe/edu/pucp/acoseg/image/ImageFileHelper.java
+++ AnotherACOImageSegmentation/src/pe/edu/pucp/acoseg/image/ImageFileHelper.java
@@ -5,6 +5,7 @@ import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 import javax.imageio.ImageIO;
 
@@ -32,12 +33,59 @@ public class ImageFileHelper {
 		return imageAsArray;
 	}
 
+	public static ImageSegment getSegmentWithoutBackground(int[][] originalImage) {
+		int grayScaleDelta = 10;
+		int yLeftBoundary = -1;
+		int yRightBoundary = -1;
+		int xTopBoundary = -1;
+		int xBottomBoundary = -1;
+
+		int previousGreyScale = originalImage[originalImage.length / 2][0];
+		for (int i = 1; i < originalImage[0].length; i++) {
+			int currentGreyScale = originalImage[originalImage.length / 2][i];
+			if (Math.abs(currentGreyScale - previousGreyScale) > grayScaleDelta
+					&& yLeftBoundary < 0) {
+				yLeftBoundary = i;
+			} else if (Math.abs(currentGreyScale - previousGreyScale) > grayScaleDelta
+					&& yLeftBoundary > 0) {
+				yRightBoundary = i;
+			}
+		}
+
+		previousGreyScale = originalImage[0][originalImage[0].length / 2];
+		for (int i = 1; i < originalImage.length; i++) {
+			int currentGreyScale = originalImage[i][originalImage[0].length / 2];
+			if (Math.abs(currentGreyScale - previousGreyScale) > grayScaleDelta
+					&& xTopBoundary < 0) {
+				xTopBoundary = i;
+			} else if (Math.abs(currentGreyScale - previousGreyScale) > grayScaleDelta
+					&& xTopBoundary > 0) {
+				xBottomBoundary = i;
+			}
+		}
+		return new ImageSegment(yLeftBoundary, yRightBoundary, xTopBoundary,
+				xBottomBoundary);
+	}
+
+	public static int[][] cropImage(ImageSegment imageSegment,
+			int[][] originalImage) {
+
+		int numberOfRows = imageSegment.getxBottomBoundary()
+				- imageSegment.getxTopBoundary() + 1;
+		int[][] croppedImage = new int[numberOfRows][];
+		for (int i = 0, j = imageSegment.getxTopBoundary(); i < numberOfRows; i++, j++) {
+			croppedImage[i] = Arrays.copyOfRange(originalImage[j],
+					imageSegment.getyLeftBoundary(),
+					imageSegment.getyRightBoundary());
+		}
+		return croppedImage;
+	}
+
 	public static void generateImageFromArray(
 			int[][] normalizedPheromoneMatrix, String outputImageFile)
 			throws IOException {
-		System.out
-				.println(ACOImageSegmentation.getComputingTimeAsString()
-						+ "Generating output image");
+		System.out.println(ACOImageSegmentation.getComputingTimeAsString()
+				+ "Generating output image");
 		BufferedImage outputImage = new BufferedImage(
 				normalizedPheromoneMatrix.length,
 				normalizedPheromoneMatrix[0].length,
@@ -51,7 +99,7 @@ public class ImageFileHelper {
 		File imageFile = new File(outputImageFile);
 		ImageIO.write(outputImage, "bmp", imageFile);
 		System.out.println(ACOImageSegmentation.getComputingTimeAsString()
-				
-				+ "Resulting image stored in: " + outputImageFile);
+
+		+ "Resulting image stored in: " + outputImageFile);
 	}
 }
