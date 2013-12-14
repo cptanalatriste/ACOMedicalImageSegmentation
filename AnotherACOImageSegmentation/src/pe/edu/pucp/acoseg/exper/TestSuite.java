8072f4612e706f07906956fb9ee44d520f99d00e
Carlos G. Gavidia <cgavidia@acm.org>
2013-12-14 17:50:57 -0500

    Performance optimization

--- AnotherACOImageSegmentation/src/pe/edu/pucp/acoseg/exper/TestSuite.java
+++ AnotherACOImageSegmentation/src/pe/edu/pucp/acoseg/exper/TestSuite.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import pe.edu.pucp.acoseg.ProblemConfiguration;
+import pe.edu.pucp.acoseg.image.ImageSegment;
 
 public class TestSuite {
 
@@ -32,8 +33,8 @@ public class TestSuite {
 		System.out.println("===============================");
 
 		for (ImageComparator comparator : comparisonList) {
-			double maximumBDP = 0;
-			String maximumBDPClusterFile = "";
+			double maximumJCI = 0;
+			String maximumJCIClusterFile = "";
 			for (int i = 0; i < ProblemConfiguration.NUMBER_OF_CLUSTERS; i++) {
 				String currentFile = ProblemConfiguration.OUTPUT_DIRECTORY + i
 						+ "_" + ProblemConfiguration.CLUSTER_IMAGE_FILE;
@@ -43,13 +44,13 @@ public class TestSuite {
 								+ "_"
 								+ ProblemConfiguration.CLUSTER_IMAGE_FILE);
 				comparator.executeComparison();
-				if (comparator.getBuildingDetectionPercentage() > maximumBDP) {
-					maximumBDP = comparator.getBuildingDetectionPercentage();
-					maximumBDPClusterFile = currentFile;
+				if (comparator.getJaccardSimilarityIndex() > maximumJCI) {
+					maximumJCI = comparator.getJaccardSimilarityIndex();
+					maximumJCIClusterFile = currentFile;
 				}
 
 			}
-			comparator.setImageToValidateFile(maximumBDPClusterFile);
+			comparator.setImageToValidateFile(maximumJCIClusterFile);
 		}
 
 		System.out.println(ProblemConfiguration.currentConfigurationAsString());
@@ -58,55 +59,6 @@ public class TestSuite {
 			comparator.executeComparison();
 			System.out.println(comparator.resultAsString());
 		}
-
-	}
-
-	public static void main(String... args) throws Exception {
-
-		System.out.println("\n\nEXPERIMENT EXECUTION REPORT");
-		System.out.println("===============================");
-
-		TestSuite testSuite = new TestSuite();
-
-		for (ImageComparator comparator : testSuite.comparisonList) {
-			double maximumBDP = 0;
-			String maximumBDPClusterFile = "";
-			for (int i = 0; i < ProblemConfiguration.NUMBER_OF_CLUSTERS; i++) {
-				String currentFile = ProblemConfiguration.OUTPUT_DIRECTORY + i
-						+ "_" + ProblemConfiguration.CLUSTER_IMAGE_FILE;
-				comparator
-						.setImageToValidateFile(ProblemConfiguration.OUTPUT_DIRECTORY
-								+ i
-								+ "_"
-								+ ProblemConfiguration.CLUSTER_IMAGE_FILE);
-				comparator.executeComparison();
-				if (comparator.getBuildingDetectionPercentage() > maximumBDP) {
-					maximumBDP = comparator.getBuildingDetectionPercentage();
-					maximumBDPClusterFile = currentFile;
-				}
-
-			}
-			comparator.setImageToValidateFile(maximumBDPClusterFile);
-		}
-
-		System.out.println(ProblemConfiguration.currentConfigurationAsString());
-
-		double maxMeanJCI = -1;
-		double maxThreshold = -1;
-
-		for (int i = 5; i <= ProblemConfiguration.GRAYSCALE_MAX_RANGE; i++) {
-			for (ImageComparator comparator : testSuite.comparisonList) {
-				comparator.setGreyScalePositiveThreshold(i);
-				comparator.executeComparison();
-				if (comparator.getJaccardSimilarityIndex() > maxMeanJCI) {
-					maxMeanJCI = comparator.getJaccardSimilarityIndex();
-					maxThreshold = i;
-				}
-			}
-		}
-
-		System.out.println("maxMeanJCI=" + maxMeanJCI + ", maxThreshold= "
-				+ maxThreshold);
 
 	}
 }
