02ae788a3b47cb746834f3d199d22d34bdb51be2
Carlos G. Gavidia <cgavidia@acm.org>
2013-12-14 17:47:08 -0500

    Performance optimizations

--- AnotherACOImageSegmentation/src/pe/edu/pucp/acoseg/ant/Ant.java
+++ AnotherACOImageSegmentation/src/pe/edu/pucp/acoseg/ant/Ant.java
@@ -1,11 +1,13 @@
 package pe.edu.pucp.acoseg.ant;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
-import pe.edu.pucp.acoseg.ACOImageSegmentation;
 import pe.edu.pucp.acoseg.ProblemConfiguration;
+import pe.edu.pucp.acoseg.cluster.Cluster;
 import pe.edu.pucp.acoseg.image.ClusteredPixel;
 import pe.edu.pucp.acoseg.image.PosibleAssignment;
 
@@ -13,13 +15,19 @@ public class Ant {
 
 	private int currentIndex = 0;
 	private int numberOfClusters;
-	private ClusteredPixel partition[];
+	private ClusteredPixel[] partition;
+	private Map<Integer, Cluster> clusterMap;
+	private double partitionQuality = -1;
 
 	// TODO(cgavidia):Visited matrix was removed because memory concerns
 
 	public Ant(int numberOfPixels, int numberOfClusters) {
 		this.partition = new ClusteredPixel[numberOfPixels];
 		this.numberOfClusters = numberOfClusters;
+		this.clusterMap = new HashMap<Integer, Cluster>();
+		for (int i = 0; i < numberOfClusters; i++) {
+			clusterMap.put(i, new Cluster(i));
+		}
 	}
 
 	public void setCurrentIndex(int currentIndex) {
@@ -28,12 +36,17 @@ public class Ant {
 
 	public void addAsignmentToSolution(ClusteredPixel visitedPixel) {
 		partition[currentIndex] = visitedPixel;
+		clusterMap.get(visitedPixel.getCluster()).getPixels().add(visitedPixel);
 		currentIndex++;
 	}
 
 	public void clear() {
+		partitionQuality = -1;
 		for (int i = 0; i < partition.length; i++) {
 			partition[i] = null;
+		}
+		for (int i = 0; i < numberOfClusters; i++) {
+			clusterMap.get(i).setPixels(new ArrayList<ClusteredPixel>());
 		}
 	}
 
@@ -73,7 +86,6 @@ public class Ant {
 		List<PosibleAssignment> probabilities = getProbabilities(xCoordinate,
 				yCoordinate, pheromoneTrails, imageGraph);
 
-		// TODO(cgavidia): Calibrate this parameter
 		if (randomValue < ProblemConfiguration.BEST_CHOICE_PROBABILITY) {
 			return getMaximumValueAssignment(probabilities);
 		} else {
@@ -123,7 +135,6 @@ public class Ant {
 		for (int cluster = 0; cluster < numberOfClusters; cluster++) {
 			ClusteredPixel currentPixel = new ClusteredPixel(xCoordinate,
 					yCoordinate, imageGraph, cluster);
-			// TODO(cgavidia): This will get real soon
 			double heuristicValue = getHeuristicValue(currentPixel, imageGraph)
 					+ ProblemConfiguration.DELTA;
 			double pheromoneTrailValue = pheromoneTrails[xCoordinate
@@ -154,6 +165,7 @@ public class Ant {
 			int[][] imageGraph) {
 		double euclideanDistance = Math.abs(currentPixel.getGreyScaleValue()
 				- getClusterMeanValue(currentPixel.getCluster()));
+		// Minor change, let's see how it goes
 		double contiguityMeasure = getContiguityMeasure(currentPixel,
 				imageGraph);
 		return euclideanDistance
@@ -162,22 +174,22 @@ public class Ant {
 	}
 
 	public double getPartitionQuality(int[][] imageGraph) {
-
-		double partitionQuality = 0.0;
-		// TODO(cgavidia): This MUST be optimized. The Cluster clas would
-		// be a great help
-		for (int cluster = 0; cluster < numberOfClusters; cluster++) {
-			int clusterCounter = 0;
-			double clusterQuality = 0;
-			for (int pixel = 0; pixel < currentIndex; pixel++) {
-				if (partition[pixel].getCluster() == cluster) {
-					clusterCounter++;
-					clusterQuality += getHeuristicValue(partition[pixel],
-							imageGraph);
+		if (partitionQuality < 0) {
+			partitionQuality = 0.0;
+			// TODO(cgavidia): This MUST be optimized. The Cluster clas would
+			// be a great help
+			for (int cluster = 0; cluster < numberOfClusters; cluster++) {
+				List<ClusteredPixel> pixelsFromCluster = clusterMap
+						.get(cluster).getPixels();
+				int clusterCounter = pixelsFromCluster.size();
+				double clusterQuality = 0;
+				for (int pixel = 0; pixel < pixelsFromCluster.size(); pixel++) {
+					clusterQuality += getHeuristicValue(
+							pixelsFromCluster.get(pixel), imageGraph);
 				}
+				partitionQuality += clusterCounter != 0 ? clusterQuality
+						/ clusterCounter : 0;
 			}
-			partitionQuality += clusterCounter != 0 ? clusterQuality
-					/ clusterCounter : 0;
 		}
 		return partitionQuality;
 	}
@@ -187,30 +199,22 @@ public class Ant {
 		// TODO(cgavidia): Contiguity measure taken from: A contiguity-enhanced
 		// k-means clustering algorithm forunsupervised multispectral image
 		// segmentation
+
 		double neighboursWithSameClass = 0;
-		for (int i = 0; i < currentIndex; i++) {
-			ClusteredPixel clusteredPixel = partition[i];
-			if (clusteredPixel.getCluster() == currentPixel.getCluster()
-					&& clusteredPixel.isNeighbour(currentPixel, imageGraph)) {
+
+		List<ClusteredPixel> neighbours = currentPixel.getNeighbourhood(
+				partition, imageGraph);
+		for (ClusteredPixel neighbour : neighbours) {
+			if (neighbour.getCluster() == currentPixel.getCluster()) {
 				neighboursWithSameClass++;
 			}
 		}
-		return currentIndex != 0 ? neighboursWithSameClass / currentIndex : 0;
+		return neighbours.size() != 0 ? neighboursWithSameClass
+				/ neighbours.size() : 0;
 	}
 
 	public double getClusterMeanValue(int cluster) {
-		// TODO(cgavidia): This should me stored to save some CPU time
-		double greyScaleSum = 0.0;
-		double clusterCounter = 0;
-		for (int i = 0; i < currentIndex; i++) {
-			ClusteredPixel clusteredPixel = partition[i];
-			if (clusteredPixel.getCluster() == cluster) {
-				greyScaleSum = greyScaleSum
-						+ clusteredPixel.getGreyScaleValue();
-				clusterCounter = clusterCounter + 1;
-			}
-		}
-		return clusterCounter != 0 ? greyScaleSum / clusterCounter : 0;
+		return clusterMap.get(cluster).getClusterMeanValue();
 	}
 
 	// TODO(cgavidia): This can also dissapear
